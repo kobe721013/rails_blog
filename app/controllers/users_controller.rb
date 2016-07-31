7c4aada6284cab3d7afcb8f074352bcb31714ae4
kobe Lin <kobebryant721013@gmail.com>
2016-08-01 06:47:43 +0800

    User table connecnt with Post table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all #翻譯成 select * from users;
   end
 
   # GET /users/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+	belongs_to :user #[kobe_notes]根據field=>user_id去與user table關聯起來
 end
